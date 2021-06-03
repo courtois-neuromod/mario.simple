72155b6c56080908bb052b330435af338640847e
basile <basile.pinsard@gmail.com>
2021-06-03 13:21:11 -0400

    update all levels

--- generate_sublevels.py
+++ generate_sublevels.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-import retro
+import retro, gzip
 
 env = retro.make('SuperMarioBros-Nes','Level1-1',inttype=retro.data.Integrations.CUSTOM)
 env.reset()
@@ -9,4 +9,6 @@ for world in range(8):
     env.data.set_value('levelHi', world)
     for stage in range(4):
         env.data.set_value('levelLo', stage)
-        env.data.save(f"SuperMarioBros-Nes/Level{world+1}-{stage+1}.state")
+        env.reset()
+        with gzip.open(f"SuperMarioBros-Nes/Level{world+1}-{stage+1}.state.tmp", 'wb') as fh:
+            fh.write(env.initial_state)
